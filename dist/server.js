8da80720b8bfed7673e5354d822e5b8a0cc093d5
Denis <sabaceagdenis@gmail.com>
2026-02-12 14:54:53 +0100

    exercices jusqu a 6.2

---
+++ dist/server.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+let name = "cucu";
+console.log(greet(name));
+//# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,18 @@
-function greet(name:string): string {
-    return `Hello, ${name}!`;
-}
-let name = "cucu";
-console.log(greet(name));
+import express, { Request,  Response }  from "express";
+
+const app = express();
+const port = 3000;
+app.get('/',(req : Request,res : Response)=> {
+    res.send('Bienvenuuuuuuue');
+});
+app.get("/api/data", (req: Request, res: Response) => {
+    res.json(etudiants);
+}) 
+app.listen(port, () => {
+    console.log('serveur lancé sur htpp://localhost:${port}');
+});
+const etudiants = [
+    { id: 1 , nom: "Dupont", prenom: "Jean"},
+    { id: 2 , nom: "Martin", prenom: "Sophie"},
+    { id: 3 , nom: "Doe", prenom: "John"}
+]
